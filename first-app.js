57e7619647e8e6a9b31232f27161127d45f080ae
Dzung Khuong <vietdungkhuong@gmail.com>
2022-12-20 10:27:18 +0700

    Lab1.3 Sending Responses

--- first-app.js
+++ first-app.js
@@ -2,7 +2,12 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req);
-  process.exit();
+  //process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
 })
 
 server.listen(3000);
